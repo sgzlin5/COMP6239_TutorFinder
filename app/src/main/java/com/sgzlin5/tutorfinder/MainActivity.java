606fe10039f93d05bac5ad204e7397dfb6eb29cb
zl4u18@soton.ac.uk <359632954@qq.com>
2019-02-26 14:23:51 +0000

    init commit

---
+++ app/src/main/java/com/sgzlin5/tutorfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sgzlin5.tutorfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
